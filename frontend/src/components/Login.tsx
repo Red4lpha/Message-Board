33273f27f2b504fef361d998e6a34972fc46f0b4
Kevin <alphalop42@gmail.com>
2022-06-24 19:06:39 -0700

    Redesigned the login page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,12 @@
-import {useState} from 'react';
+import {FormEvent, useState} from 'react';
 import { useAppDispatch } from '../app/hooks';
 import { login} from '../features/auth/authSlice'
 import { userDataInterface } from '../types/types';
-import {FormControl, InputLabel, Input, Button, Paper} from '@mui/material';
+import {Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -13,8 +17,9 @@ const Login = () => {
   const {email, password} = formData;
   const dispatch = useAppDispatch();
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    
     const userData: userDataInterface = {
       email,
       password,
@@ -30,32 +35,61 @@ const Login = () => {
   }
 
   return (
-    <div className="App">
-    <Paper>
-      <h1>LOGIN</h1>
-      <div style={{marginBottom: '15px'}}>
-        <FormControl>
-          <InputLabel htmlFor="email">Email address</InputLabel>
-          <Input id="email"
-          name='email' 
-          onChange={onChange}
-          value={email}
-          aria-describedby="Email Address Form" />
-        </FormControl>
-      </div>
-      <div style={{marginBottom: '15px'}}>
-        <FormControl>
-          <InputLabel htmlFor="password">password</InputLabel>
-          <Input id="password"
-          name='password' 
-          onChange={onChange}
-          value={password}
-          aria-describedby="Password Form" />
-        </FormControl>
-      </div>
-      <Button onClick={handleSubmit}>LOGIN</Button>
-    </Paper>
-  </div>
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={onChange}
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={onChange}
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+
+    </Container>
+  </ThemeProvider>
   )
 }
 
